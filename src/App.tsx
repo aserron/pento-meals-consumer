37c63bf9d5bff076743a6740fb0deb2570a6f06a
Andres Serron <aserron.dev@gmail.com>
2024-02-15 03:09:17 -0300

    - improved error display - title typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import RecipeList from "./components/RecipeList";
 
@@ -7,7 +6,7 @@ function App() {
     return (
         <div className="App" style={{display: 'hidden'}}>
             <header className="App-header">
-                <h1>Meal API Consumter</h1>
+                <h1>Meals API Consumter</h1>
             </header>
             <section className={`App-search`}>
                 <RecipeList/>
